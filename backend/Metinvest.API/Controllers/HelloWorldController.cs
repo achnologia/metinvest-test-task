3bb2a1a75cf1ae635305989bb33998be6b6d3b9d
m.kopytinlive@gmail.com <m.kopytinlive@gmail.com>
2022-06-04 10:54:16 +0300

    Basic configuration of the project

---
+++ backend/Metinvest.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Metinvest.API.Controllers;
+
+[Route("api/[controller]")]
+public class HelloWorldController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetHelloWorld()
+    {
+        return Ok(await Task.FromResult("Hello World!"));
+    }
+}
