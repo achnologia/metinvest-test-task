b09405021056243771b3eadfe1df7d0c3d340bc9
m.kopytinlive@gmail.com <m.kopytinlive@gmail.com>
2022-06-04 11:05:09 +0300

    Configured EF Core and docker db

---
+++ backend/Metinvest.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Metinvest.Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Metinvest.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("Default"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+        });
+
+        return services;
+    }
+}
---
+++ backend/Metinvest.Infrastructure/EFCore/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Metinvest.Infrastructure.EFCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        :base(options)
+    { }
+}
